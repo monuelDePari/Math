efa06decb374e474d25e721755ffecdcc041c022
monuelDePari <roman.lynnyk.sa@gmail.com>
2019-10-20 22:34:25 +0300

    added 4-th task

---
+++ MathLab4/MathLab4/Task1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLab4
+{
+    class Task1
+    {
+        List<double> XValueList = new List<double>();
+        public int Calculate()
+        {
+            Console.WriteLine("x = tanx");
+            int numberOfIterations = 0;
+            double i = -1.0;
+            bool ifIsTrue = true;
+            var x = Math.Round(i, 3);
+            while (ifIsTrue)
+            {
+                if(x >= 1.0)
+                {
+                    ifIsTrue = false;
+                }
+                if(Math.Round(Math.Tan(x), 2) - Math.Round(x, 2) == 0)
+                {
+                    numberOfIterations += 1;
+                    XValueList.Add(Math.Round(x, 3));
+                }
+                x += 0.01;
+            }
+            return numberOfIterations;
+        }
+        public void Run()
+        {
+            Console.WriteLine($"amount of the roots - {Calculate()}");
+            foreach (var item in XValueList)
+            {
+                Console.WriteLine($"x = {item}");
+            }
+        }
+    }
+}
---
+++ MathLab4/MathLab4/Task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLab4
+{
+    class Task2
+    {
+        double fp(double x) { return -3 * Math.Pow(x, 2) + 4 * x; }
+        public void Run()
+        {
+            double a, b;
+            Console.WriteLine("Input start of range");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input end of range ");
+            b = Convert.ToDouble(Console.ReadLine());
+            double e, i = 0;
+            Console.WriteLine("Enter e");
+            e = Convert.ToDouble(Console.ReadLine());
+            if (Math.Abs(fp(a)) < a || Math.Abs(fp(a)) > b || Math.Abs(fp(b)) < a || Math.Abs(fp(b)) > b)
+                Console.WriteLine("Error");
+            else
+            {
+                double x, x1;
+                x1 = 0.01;
+                do
+                {
+                    x = x1;
+                    Console.WriteLine($"{i}\t{x}\t");
+                    x1 = -3 * x * x + 4 * x;
+                    Console.WriteLine($"{x1}");
+                    i++;
+                } while (Math.Abs(x1 - x) > e);
+            }
+            Console.Read();
+        }
+    }
+}
