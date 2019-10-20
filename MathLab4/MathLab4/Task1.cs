09f30ed7e221b270c8db60277cd23540ef81dc38
monuelDePari <roman.lynnyk.sa@gmail.com>
2019-10-21 00:50:26 +0300

    changed homework 4 Task1

--- MathLab4/MathLab4/Task1.cs
+++ MathLab4/MathLab4/Task1.cs
@@ -9,34 +9,36 @@ namespace MathLab4
     class Task1
     {
         List<double> XValueList = new List<double>();
-        public int Calculate()
+        public void Calculate()
         {
             Console.WriteLine("x = tanx");
-            int numberOfIterations = 0;
             double i = -1.0;
             bool ifIsTrue = true;
-            var x = Math.Round(i, 3);
+            var x = Math.Round(i, 3); 
             while (ifIsTrue)
             {
                 if(x >= 1.0)
                 {
                     ifIsTrue = false;
                 }
-                if(Math.Round(Math.Tan(x), 2) - Math.Round(x, 2) == 0)
+                if(Math.Round(Math.Tan(x), 3) - Math.Round(x, 3) == 0)
                 {
-                    numberOfIterations += 1;
                     XValueList.Add(Math.Round(x, 3));
                 }
                 x += 0.01;
             }
-            return numberOfIterations;
         }
         public void Run()
         {
-            Console.WriteLine($"amount of the roots - {Calculate()}");
-            foreach (var item in XValueList)
+            Calculate();
+            var PositiveXValueList = from t in XValueList
+                                     where t >= 0
+                                     orderby t
+                                     select t;
+            Console.WriteLine($"amount of the roots - {PositiveXValueList.Count()}");
+            foreach (var item in PositiveXValueList)
             {
-                Console.WriteLine($"x = {item}");
+                Console.WriteLine(item);
             }
         }
     }
